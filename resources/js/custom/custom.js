ab405d112c59c38a55af9286c9cf6ec8d3055fba
Fernando Shinji Washimi <fernandowashimi@gmail.com>
2017-11-05 01:54:06 -0200

    Ajustes no background e navbar

--- resources/js/custom/custom.js
+++ resources/js/custom/custom.js
@@ -1,6 +1,31 @@
-$(document).ready(function() {
-  $('#quote').carousel({
-    pause: true,
-    interval: 4000,
-  });
+function hex2rgba(hex, opacity)
+{
+    //extract the two hexadecimal digits for each color
+    var patt = /^#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})$/;
+    var matches = patt.exec(hex);
+
+    //convert them to decimal
+    var r = parseInt(matches[1], 16);
+    var g = parseInt(matches[2], 16);
+    var b = parseInt(matches[3], 16);
+
+    //create rgba string
+    var rgba = "rgba(" + r + "," + g + "," + b + "," + opacity + ")";
+
+    //return rgba colour
+    return rgba;
+}
+
+$(window).scroll(function() {
+    if($(this).scrollTop() > 60)
+    {
+        $(".navbar").css("background-color", hex2rgba('#ffa726', 0.8));
+        $(".navbar").css("transition", ".5s ease");
+        $("#btnLogin").css("border-color", "#FFF");
+        $("#btnLogin").css("color", "#FFF");
+    } else {
+        $(".navbar").css("background-color", hex2rgba('#ffa726', 0));
+        $("#btnLogin").css("border-color", "#ffa726");
+        $("#btnLogin").css("color", "#ffa726");
+    }
 });
